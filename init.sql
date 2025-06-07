eed3582d4b3ad340226db03c2b43f4ade04ad5a7
Hemerson Daniel <adv.hemersondaniel@hotmail.com>
2025-06-07 17:28:19 -0300

    Adicionei script para criar tabela usuarios

---
+++ init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS usuarios (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  senha VARCHAR(255) NOT NULL,
+  role VARCHAR(50) NOT NULL DEFAULT 'aluno',
+  nome VARCHAR(255) NOT NULL
+);
+
+INSERT INTO usuarios (nome, email, senha, role) VALUES
+('Administrador', 'admin@buriti.com', '$2b$10$Z1YHxw691G3UE9OQP1i1m.B1hb.3H5QB1uoDmkYMOtHYQmi16TK.a', 'admin'),
+('Hemerson Daniel Teste Atualizado 4', 'joao@aluno.com', '$2b$10$Z1YHxw691G3UE9OQP1i1m.B1hb.3H5QB1uoDmkYMOtHYQmi16TK.a', 'aluno')
+ON CONFLICT (email) DO NOTHING;
