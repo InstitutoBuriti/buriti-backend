cc9c69300ac0566acfc6d27616a10678c5304243
Hemerson Daniel <adv.hemersondaniel@hotmail.com>
2025-06-07 17:41:34 -0300

    Adicionei criação da tabela cursos no init.sql

--- init.sql
+++ init.sql
@@ -10,3 +10,14 @@ INSERT INTO usuarios (nome, email, senha, role) VALUES
 ('Administrador', 'admin@buriti.com', '$2b$10$Z1YHxw691G3UE9OQP1i1m.B1hb.3H5QB1uoDmkYMOtHYQmi16TK.a', 'admin'),
 ('Hemerson Daniel Teste Atualizado 4', 'joao@aluno.com', '$2b$10$Z1YHxw691G3UE9OQP1i1m.B1hb.3H5QB1uoDmkYMOtHYQmi16TK.a', 'aluno')
 ON CONFLICT (email) DO NOTHING;
+
+CREATE TABLE IF NOT EXISTS cursos (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  descricao TEXT,
+  modality VARCHAR(50),
+  duration VARCHAR(10),
+  price DECIMAL(10, 2),
+  status VARCHAR(20) DEFAULT 'Rascunho',
+  imagem VARCHAR(255)
+);
